fb9abf160a94b5de5352a9978044d58f6425cda5
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-05-06 12:55:36 -0600

    Add font-medium to description term component

--- resources/views/components/description/term.blade.php
+++ resources/views/components/description/term.blade.php
@@ -2,7 +2,7 @@
     {{ $attributes->except('class') }}
     @class([
         $attributes->get('class'),
-        'col-start-1 border-t border-zinc-950/5 pt-2 text-zinc-950 first:border-none sm:border-t sm:border-zinc-950/5 sm:py-2 dark:border-white/5 dark:text-white sm:dark:border-white/5',
+        'font-medium col-start-1 border-t border-zinc-950/5 pt-2 text-zinc-950 first:border-none sm:border-t sm:border-zinc-950/5 sm:py-2 dark:border-white/5 dark:text-white sm:dark:border-white/5',
     ])
 >
     {{ $slot }}
