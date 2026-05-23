30d28c129a0d99f09da3f1ad464fbc30365edb87
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-05-22 18:21:37 -0600

    Update dashboard styling to use zinc color palette

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,18 +1,18 @@
 <x-layouts::app title="Dashboard">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+    <div class="flex flex-col gap-4">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div class="relative aspect-video overflow-hidden border border-zinc-950/10 dark:border-white/10">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-zinc-950/5 dark:stroke-white/5" />
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div class="relative aspect-video overflow-hidden border border-zinc-950/10 dark:border-white/10">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-zinc-950/5 dark:stroke-white/5" />
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div class="relative aspect-video overflow-hidden border border-zinc-950/10 dark:border-white/10">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-zinc-950/5 dark:stroke-white/5" />
             </div>
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        <div class="relative aspect-video overflow-hidden border border-zinc-950/10 dark:border-white/10">
+            <x-placeholder-pattern class="absolute inset-0 size-full stroke-zinc-950/5 dark:stroke-white/5" />
         </div>
     </div>
 </x-layouts::app>
